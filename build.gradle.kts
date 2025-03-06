769e80ac2fb67a38197b29f8b203bdd519d14cf7
Simon Wächter <waechter.simon@gmail.com>
2025-03-06 20:06:23 +0100

    Add the example code

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "ch.swaechter"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Switch to JUnit version 5.12.0 to reproduce the error
+    //testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation(platform("org.junit:junit-bom:5.12.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/swaechter/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.swaechter;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void testHello() {
+        System.out.println("Hello World");
+    }
+}
